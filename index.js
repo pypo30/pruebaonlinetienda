12cf884ecebd25ac6664bc870b3054a4712a2169
pypo30 <42309093+pypo30@users.noreply.github.com>
2023-07-31 21:46:11 -0300

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const url = 'https://json-server--3000.local.webcontainer.io/comments';
+require('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+connection.end();
 
 fetch(url)
-  .then(r => r.json())
-  .then(json => (document.getElementById('output').innerHTML = JSON.stringify(json, null, 2)));
+  .then((r) => r.json())
+  .then(
+    (json) =>
+      (document.getElementById('output').innerHTML = JSON.stringify(
+        json,
+        null,
+        2
+      ))
+  );
